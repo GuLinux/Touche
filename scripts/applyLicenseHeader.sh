d1b48aa04960fe95b419eab5a01432244c091db8
Marco Gulino <marco.gulino@gmail.com>
2012-05-03 19:23:05 +0200

    Copyright header in source files

---
+++ scripts/applyLicenseHeader.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HEADER_BEGIN="/***********************************************************************"
+HEADER_END="***********************************************************************/"
+
+
+function applyToFile() {
+	file="$1"
+	tempFile="/tmp/$( basename $0).tmp.cxx"
+	echo "Applying license header to $file"
+	LICENSE_START_AT="$( cat "$file" | grep -n -F "$HEADER_END" | cut -d: -f1 )"
+	if test "x$LICENSE_START_AT" == "x"; then LICENSE_START_AT="-2"; fi
+	echo "$HEADER_BEGIN" > "$tempFile"
+	cat "$( dirname $0)/LicenseHeader.txt" >> "$tempFile"
+	echo -e "$HEADER_END\n" >> "$tempFile"
+	cat "$file" | tail -n +$(($LICENSE_START_AT+2)) >> "$tempFile"
+	cat "$tempFile" > "$file"
+}
+
+function findAll() {
+	find -type f \( -name "*.cpp" -or -name "*.h" \) -and -not -name "moc_*.cpp" -exec "$0" applyTo {} \;
+}
+
+
+case "$1" in
+	"applyTo")
+	applyToFile "$2"
+	;;
+	*)
+	findAll
+	;;
+esac
