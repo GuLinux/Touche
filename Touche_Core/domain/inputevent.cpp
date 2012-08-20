bff270d6ec3dd22a577057d6a97a3e2105b12c30
Marco Gulino <marco.gulino@gmail.com>
2012-08-20 17:44:36 +0200

    Found an unused class, removing

--- Touche_Core/domain/inputevent.cpp
+++ Touche_Core/domain/inputevent.cpp
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "inputevent.h"
 #include <QMultiMap>
-#include "inputregister.h"
 #include <QDebug>
 #include <QStringList>
 #include <QPair>
--- Touche_Core/domain/inputevent.h
+++ Touche_Core/domain/inputevent.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QList>
 
 class InputEventPrivate;
-class InputRegister;
 
 typedef QPair<uint, uint> RegisterValue;
 
